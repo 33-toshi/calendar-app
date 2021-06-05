2e672ec51922187d534a9fab97b7fa23ac2e1399
33-toshi <hatta.satoshi.s610514@gmail.com>
2021-06-05 13:56:17 +0900

    firstcommit

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,43 @@
+class PlansController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @plans = Plan.order("start_time DESC")
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.valid?
+      @plan.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    plan = Plan.find(params[:id])
+    plan.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @plan = Plan.find(params[:id])
+  end
+
+  def update
+    plan = Plan.find(params[:id])
+    plan.update(plan_params)
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:task, :detail, :memo, :start_time).merge(user_id: current_user.id)
+  end
+  
+end
---
+++ db/migrate/20210605034300_create_plans.rb
@@ -0,0 +1,12 @@
+class CreatePlans < ActiveRecord::Migration[6.0]
+  def change
+    create_table :plans do |t|
+      t.string        :task
+      t.text          :detail
+      t.text          :memo
+      t.datetime      :start_time
+      t.references    :user,     foreign_key: true
+      t.timestamps
+    end
+  end
+end
